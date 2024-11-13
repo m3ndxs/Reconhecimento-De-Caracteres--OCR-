0cb57ebc8ac612d46cc685a37af3bc7c0ea7723e
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2024-11-13 13:23:29 -0300

    Adicionado tesseract reconhecimento imagens

--- interface.py
+++ interface.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import textwrap
+import ocr_tesseract
+
 from tkinter import filedialog, messagebox
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
@@ -7,8 +9,23 @@ from docx import Document
 
 
 def recognize_image():
-    text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend purus in turpis facilisis convallis. Curabitur nec consectetur felis. Aliquam.'
+    image_path = filedialog.askopenfilename(
+        filetypes=[("Arquivos de Imagem", "*.png;*.jpg;*.jpeg;*.bmp")])
+    if not image_path:
+        return
 
+    text = ocr_tesseract.recognize_text_from_image(image_path)
+    display_result(text)
+
+
+def recognize_pdf():
+    pdf_path = filedialog.askopenfilename(
+        filetypes=[("Arquivos PDF", "*.pdf")])
+
+    if not pdf_path:
+        return
+
+    text = ocr_tesseract.recognize_text_from_pdf(pdf_path)
     display_result(text)
 
 
@@ -25,17 +42,35 @@ def save_text(text, file_format):
                 file.write(text)
         elif file_format == "pdf":
             c = canvas.Canvas(save_path, pagesize=letter)
-            lines = wrapped_text.split("\n")
-            y = 750
+            width, height = letter
+
+            lines = []
+            for paragraph in text.split('\n'):
+                wrapped_lines = textwrap.wrap(paragraph, width=60)
+                lines.extend(wrapped_lines)
+
+            x = 50
+            y = height - 50
+
             for line in lines:
-                c.drawString(100, y, line)
+                if y < 50:
+                    c.showPage()
+                    y = height - 50
+                c.drawString(x, y, line)
                 y -= 15
+
             c.save()
+
         elif file_format == "docx":
             doc = Document()
-            for line in wrapped_text.split("\n"):
-                doc.add_paragraph(line)
+
+            for paragraph in text.split('\n'):
+                wrapped_lines = textwrap.wrap(paragraph, width=60)
+                for line in wrapped_lines:
+                    doc.add_paragraph(line)
+
             doc.save(save_path)
+
         elif file_format == "html":
             with open(save_path, "w", encoding="utf-8") as file:
                 file.write(f"<html><body><pre>{text}</pre></body></html>")
@@ -85,7 +120,7 @@ btn_image = tk.Button(button_frame, text='Imagem', command=recognize_image,
                       bg='skyblue', fg='white', font=('Arial', 12, 'bold'))
 btn_image.grid(padx=20, pady=10)
 
-btn_pdf = tk.Button(button_frame, text='PDF', command=recognize_image,
+btn_pdf = tk.Button(button_frame, text='PDF', command=recognize_pdf,
                     bg='lightgreen', fg='black', font=('Arial', 12, 'bold'))
 btn_pdf.grid(padx=20, pady=10)
 
---
+++ ocr_tesseract.py
@@ -0,0 +1,32 @@
+import pytesseract
+from pdf2image import convert_from_path
+from PIL import Image
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+def recognize_text_from_image(image_path):
+    try:
+        image = Image.open(image_path)
+        text = pytesseract.image_to_string(image, lang='por')
+
+        return text
+
+    except Exception as e:
+        return f"Erro ao carregar a Imagem: {e}"
+
+
+def recognize_text_from_pdf(pdf_path):
+    try:
+        pages = convert_from_path(pdf_path)
+        extracted_text = ""
+
+        for page in pages:
+            text = pytesseract.image_to_string(page, lang='por')
+            extracted_text += text + "\n"
+
+        return extracted_text
+
+    except Exception as e:
+        return f"Erro ao carregar o PDF: {e}"
